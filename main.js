888d821c9c6fba053865c5d29ab6cb1e88fde2b9
Broken__System <70167960+ShellDredd@users.noreply.github.com>
2021-09-14 22:06:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="texto1">Freedom User</i>',
+		'<i class="texto2">Content Creator</i>',
+		'<i class="texto3">Cybersecurity Enthusiast</i>',
+		'<i class="texto1">Player and Creator of CTFs</i>',
+		'<i class="texto2">Supporter of the FOSS ecosystem</i>',
+		'<i class="texto3">Gnu/Linux Disseminator</i>',
+		'<i class="texto1">Founder of the ShellDredd Society</i>',
+		'<i class="texto2">SysAdmin - Front-End Web Designer</i>',
+	],	
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 50, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
